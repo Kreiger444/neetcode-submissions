f29a42171882e535b6e802c184e30a4debbf993d
Virinchi Mourya <75578135+Kreiger444@users.noreply.github.com>
2026-04-19 15:26:07 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string result;
+
+        for (const auto& str : strs) {
+            result += static_cast<char>(str.size() & 0xff);
+            result += str;
+        }
+
+        return result;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+
+        for (size_t i = 0; i < s.size(); ) {
+            const size_t length = static_cast<size_t>(
+                static_cast<unsigned char>(s[i]));
+            result.push_back(s.substr(i + 1, length));
+            i += length + 1;
+        }
+
+        return result;
+    }
+};
